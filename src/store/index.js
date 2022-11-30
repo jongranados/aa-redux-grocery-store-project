f5408017d72305edfc3715ac5f95550cac18ed7c
Jon Granados <grxnxdos@gmail.com>
2022-11-29 17:36:43 -0800

    Redux store configured and connected to React app.

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import {createStore, combineReducers, applyMiddleware, compose } from 'redux'; 
+
+const rootReducer = combineReducers({ 
+   
+}); 
+
+let enhancer; 
+
+if (process.env.NODE_ENV !== 'production') { 
+    const logger = require('redux-logger').default;
+    const composeEnhancers = 
+        typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? 
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) : compose; 
+    
+    // enhancer is set to the combined store enhancer
+    enhancer = composeEnhancers(applyMiddleware(logger)); 
+}
+
+function configureStore(preloadedState) { 
+    return createStore(rootReducer, preloadedState, enhancer);
+}; 
+
+export default configureStore; 
